6ff98c9765f2cff9ab918d81c3e3a8949e0fbff5
Mike Turley <mike@miketurley.com>
2014-04-23 00:37:41 -0400

    directories.  (needs path fixing)

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  // this will run on every page
+  $("#please-wait").modal({
+    keyboard: false,
+    show: false
+  });
+
+  var body = $(body);
+
+  if(body.hasClass('register-page') {
+    // on the new user registration page
+
+    $("#submit").click(function(e) {
+      // submit the registration form data via ajax
+      e.preventDefault();
+      var postdata = $("#registration-form").serializeObject();
+      postdata.fn = 'register_new_user';
+      $("#please-wait").modal('show');
+      $.ajax({
+        type: 'POST',
+        url: 'backend.php',
+        data: postdata,
+        success: function(response) {
+          console.log(arguments);
+          $("#please-wait").modal('hide');
+        },
+        error: function(response) {
+          console.log(arguments);
+          $("#please-wait").modal('hide');
+        }
+      });
+    });
+
+    $("#cancel").click(function(e) {
+      e.preventDefault();
+      document.location = 'musicnet.php'; // go home
+    });
+
+  }); // body.hasClass('register')
+});

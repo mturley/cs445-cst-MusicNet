041746fc910f9bbe93f9dfcefee1410448152358
Mike Turley <mike@miketurley.com>
2014-04-23 01:27:14 -0400

    stuff

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
 
+  window.cl = new CanvasLoader('canvasloader-container');
+  cl.setDiameter(32); // default is 40
+	cl.setRange(0.6); // default is 1.3
+
+
   $("#register-submit").click(function(e) {
     // submit the registration form data via ajax
     e.preventDefault();
     var postdata = $("#registration-form").serializeObject();
     postdata.fn = 'register_new_user';
-    $("#please-wait").show(); // show loading indicator
+    cl.show(); // show loading indicator
     $.ajax({
       type: 'POST',
       url: 'backend.php',
       data: postdata,
       success: function(response) {
         console.log(arguments);
-        $("#please-wait").hide(); // hide loading indicator
+        cl.hide(); // hide loading indicator
       },
       error: function(response) {
         console.log(arguments);
-        $("#please-wait").hide(); // hide loading indicator
+        cl.show(); // hide loading indicator
       }
     });
   });

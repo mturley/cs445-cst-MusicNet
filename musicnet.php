e15e58361d41b9c0ca917bce17f923a53d67dbd4
Mike Turley <mike@miketurley.com>
2014-04-23 17:23:57 -0400

    copy

--- musicnet.php
+++ musicnet.php
@@ -128,26 +128,31 @@
           </h3>
         </div>
       </div>
-
-      <div class="container">
-        <div class="row feature-buttons">
-          <div class="col-md-4">
-            <a class="btn btn-block btn-primary" href="musicnet.php?page=search" role="button">
-              Search for Music
-            </a>
+      <?php
+        if($logged_in) {
+      ?>
+          <div class="container">
+            <div class="row feature-buttons">
+              <div class="col-md-4">
+                <a class="btn btn-block btn-primary" href="musicnet.php?page=search" role="button">
+                  Search for Music
+                </a>
+              </div>
+              <div class="col-md-4">
+                <a class="btn btn-block btn-info" href="musicnet.php?page=friends" role="button">
+                  Make Friends
+                </a>
+             </div>
+              <div class="col-md-4">
+                <a class="btn btn-block btn-default" href="musicnet.php?page=concerts" role="button">
+                  Find Concerts
+                </a>
+              </div>
+            </div>
           </div>
-          <div class="col-md-4">
-            <a class="btn btn-block btn-info" href="musicnet.php?page=friends" role="button">
-              Make Friends
-            </a>
-         </div>
-          <div class="col-md-4">
-            <a class="btn btn-block btn-default" href="musicnet.php?page=concerts" role="button">
-              Find Concerts
-            </a>
-          </div>
-        </div>
-      </div>
+      <?php
+        }
+      ?>
 
       <div class="container">
         <!-- Example row of columns -->

0fa7c4d907744a776d0775319492a74a8f8ec542
Mike Turley <mike@miketurley.com>
2014-04-23 01:05:57 -0400

    typo

--- js/main.js
+++ js/main.js
@@ -5,38 +5,32 @@ $(document).ready(function() {
     show: false
   });
 
-  var body = $(body);
+  console.log("HAPPENING!!");
 
-  if(body.hasClass('register-page')) {
-    // on the new user registration page
-
-    console.log("WE'RE ON REGISTER PAGE");
-
-    $("#submit").click(function(e) {
-      // submit the registration form data via ajax
-      e.preventDefault();
-      var postdata = $("#registration-form").serializeObject();
-      postdata.fn = 'register_new_user';
-      $("#please-wait").modal('show');
-      $.ajax({
-        type: 'POST',
-        url: 'backend.php',
-        data: postdata,
-        success: function(response) {
-          console.log(arguments);
-          $("#please-wait").modal('hide');
-        },
-        error: function(response) {
-          console.log(arguments);
-          $("#please-wait").modal('hide');
-        }
-      });
+  $("#submit").click(function(e) {
+    // submit the registration form data via ajax
+    e.preventDefault();
+    var postdata = $("#registration-form").serializeObject();
+    postdata.fn = 'register_new_user';
+    $("#please-wait").modal('show');
+    $.ajax({
+      type: 'POST',
+      url: 'backend.php',
+      data: postdata,
+      success: function(response) {
+        console.log(arguments);
+        $("#please-wait").modal('hide');
+      },
+      error: function(response) {
+        console.log(arguments);
+        $("#please-wait").modal('hide');
+      }
     });
+  });
 
-    $("#cancel").click(function(e) {
-      e.preventDefault();
-      document.location = 'musicnet.php'; // go home
-    });
+  $("#cancel").click(function(e) {
+    e.preventDefault();
+    document.location = 'musicnet.php'; // go home
+  });
 
-  } // body.hasClass('register-page')
 });

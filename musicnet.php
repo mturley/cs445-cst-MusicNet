fff956ea89d5c9de6b002c1c24c0425ce89be315
Mike Turley <mike@miketurley.com>
2014-04-23 20:40:06 -0400

    icon

--- musicnet.php
+++ musicnet.php
@@ -46,8 +46,7 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="musicnet.php">
-            <span class="glyphicon glyphicon-music"></span>&nbsp;
-            <strong>MusicNet&nbsp;&nbsp;|&nbsp;&nbsp;</strong>All the Music. All the Net.
+            <strong>MusicNet&nbsp;<span class="glyphicon glyphicon-music"></span>&nbsp;</strong>All the Music. All the Net.
           </a>
           <div class="please-wait" id="floatingBarsG" style="display: none;">
             <div class="blockG" id="rotateG_01">

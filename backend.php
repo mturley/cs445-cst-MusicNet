18d0c253e2ad62597b91aa63b705b30201e12f52
Mike Turley <mike@miketurley.com>
2014-04-28 15:24:19 -0400

    print

--- backend.php
+++ backend.php
@@ -134,6 +134,7 @@
       $response->message = "Search Successful";
       $response->page = $_GET['page'];
       $response->results = $q->fetchAll();
+      print_r($response->results);
     } catch(PDOException $e) {
       $response->message = "Failed to Select from the Songs table!";
       $response->details = $e->getMessage();
@@ -156,6 +157,7 @@
       $response->message = "Search Successful";
       $response->page = $_GET['page'];
       $response->results = $q->fetchAll();
+      print_r($response->results);
     } catch(PDOException $e) {
       $response->message = "Failed to Select from the Artists table!";
       $response->details = $e->getMessage();
@@ -178,6 +180,7 @@
       $response->message = "Search Successful";
       $response->page = $_GET['page'];
       $response->results = $q->fetchAll();
+      print_r($response->results);
     } catch(PDOException $e) {
       $response->message = "Failed to Select from the Albums table!";
       $response->details = $e->getMessage();

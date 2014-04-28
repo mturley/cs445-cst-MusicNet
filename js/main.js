42dd86c4c7ee432cd694ff8c780672258d100242
Mike Turley <mike@miketurley.com>
2014-04-28 13:43:08 -0400

    keycode

--- js/main.js
+++ js/main.js
@@ -115,7 +115,7 @@ $(document).ready(function() {
     $("#search-form").on('submit', function(e) {
       e.preventDefault();
       console.log("Search for "+$("#searchinput").val());
-      $("#please-wait").show();
+      $(".please-wait").show();
       $(".search-margin").animate({ height: 0 }, { complete: function() {
         $(".search-margin").addClass('hidden');
       } });

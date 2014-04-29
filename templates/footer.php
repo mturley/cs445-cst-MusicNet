e9128f357f3940b57a35c55bbdac5f61cc7d2079
Mike Turley <mike@miketurley.com>
2014-04-28 22:17:14 -0400

    search dry

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,3 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php echo RES_PATH; ?>/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
-
-<script src="<?php echo RES_PATH; ?>/js/vendor/bootstrap.min.js"></script>
-
-<script src="<?php echo RES_PATH; ?>/js/plugins.js"></script>
-<script src="<?php echo RES_PATH; ?>/js/main.js"></script>
-
 <?php
   if($logged_in) {
 ?>
@@ -14,36 +6,17 @@
       <p><center><div id="ads">Loading Ads...</div></center></p>
     </div>
 
-    <script>
-      $(document).ready(function() {
-
-        // load the profile data for the logged in user
-
-        $(".please-wait").show();
-
-        $.ajax({
-          type: 'GET',
-          url: 'backend.php',
-          data: {
-            fn: 'get_user_by_id',
-            user_id: '<?php echo $_SESSION['user_id'] ?>'
-          },
-          success: function(response) {
-            $(".please-wait").hide();
-            var r = $.parseJSON(response);
-            $(".data-username").html(r.username);
-          },
-          error: function(response) {
-            $(".please-wait").hide();
-            bootbox.alert("Failed to load user data!  Error Message: "+$.parseJSON(response.responseText).message);
-          }
-        });
-
-      });
-    </script>
 <?php
   }
 ?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php echo RES_PATH; ?>/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+<script src="<?php echo RES_PATH; ?>/js/vendor/bootstrap.min.js"></script>
+
+<script src="<?php echo RES_PATH; ?>/js/plugins.js"></script>
+<script src="<?php echo RES_PATH; ?>/js/main.js"></script>
 
 </body>
 </html>

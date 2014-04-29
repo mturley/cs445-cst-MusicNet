0790f6f32a8e7ff7634ce5fbcff2e4b12bd93b66
Mike Turley <mike@miketurley.com>
2014-04-28 20:41:11 -0400

    stuff

--- templates/header.php
+++ templates/header.php
@@ -42,8 +42,8 @@
             All the Music.&nbsp;&nbsp;
             <span class="glyphicon glyphicon-globe"></span>
             All the Net.
+            <span class="btn btn-primary">Home</a>
           </a>
-          <a href="musicnet.php" class="btn btn-primary navbar-left">Home</a>
           <div class="please-wait" id="floatingBarsG" style="display: none;">
             <div class="blockG" id="rotateG_01">
             </div>

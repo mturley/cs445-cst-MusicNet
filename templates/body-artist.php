4c235bff1bd1b62f50dc1a0194311ceed6f12d49
Mike Turley <mike@miketurley.com>
2014-04-29 13:48:11 -0400

    stuff

--- templates/body-artist.php
+++ templates/body-artist.php
@@ -4,6 +4,10 @@
     <h2>
       <img src="<?php echo RES_PATH; ?>/img/artist_icon.png" class="obj-icon-lg" />
       View Artist: <strong class="artist_name">...</strong>
+      <a class="btn btn-primary pull-right" href="musicnet.php?page=search">
+        <span class="glyphicon glyphicon-search"></span>
+        New Search
+      </a>
     </h2>
   </div>
 </div>

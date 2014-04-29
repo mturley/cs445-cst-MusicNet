5c08dbe020b0855a3ec90f21bcc83d60144a346a
Mike Turley <mike@miketurley.com>
2014-04-28 22:30:06 -0400

    session

--- js/main.js
+++ js/main.js
@@ -109,7 +109,7 @@ $(document).ready(function() {
               $(page_row_html).appendTo($tbody);
               $("#search-results").slideDown();
               $("body").stop(); // stop scrolling if already scrolling
-              $.scrollTo("#search-results", 800, { offset: -80 });
+              $.scrollTo("#search-results", 400, { offset: -80 });
             }
             $(".please-wait").hide();
           },

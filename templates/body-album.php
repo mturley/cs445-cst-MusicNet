7deb202fe37d726bcf9ebd1070eadb6750812de8
Mike Turley <mike@miketurley.com>
2014-04-29 01:13:28 -0400

    adding body pages

---
+++ templates/body-album.php
@@ -0,0 +1,15 @@
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron slim">
+  <div class="container">
+    <h2>View Album: <?php echo $_GET['album_id']; ?></h2>
+  </div>
+</div>
+
+<div class="container">
+	<table><tr><td width="250px">
+		<div id="album-info">
+			Loading...
+		</div>
+	</td></tr></table>
+</div>
+<br><br>

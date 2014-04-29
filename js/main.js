0e454b92ec9ff120ccb4f8300dc7df48ee19c378
Mike Turley <mike@miketurley.com>
2014-04-29 03:00:04 -0400

    stuff

--- js/main.js
+++ js/main.js
@@ -124,7 +124,7 @@ $(document).ready(function() {
               $(page_row_html).appendTo($tbody);
               $("#search-results").slideDown();
               $("body").stop(); // stop scrolling if already scrolling
-              $.scrollTo("#searchinput", 400, { offset: -50 });
+              $.scrollTo("#searchinput", 400, { offset: -70 });
             }
             $(".please-wait").hide();
           },

72e8423800b4d39576be9e115d0a08fd5e2d8b82
xianc <xianc@student.umass.edu>
2014-04-28 23:56:48 -0400

    more search concerts func

--- templates/body-concerts.php
+++ templates/body-concerts.php
@@ -2,16 +2,16 @@
   <div class="container">
     <h2 class="floatleft">Find Concerts</h2>
     <div id="search-type" class="btn-group btn-group-lg floatleft">
-    <button type="button" class="btn btn-primary" data-search-type="artist">Search by Artist</button>
-    <button type="button" class="btn btn-default" data-search-type="location">Search by Location</button>
-    <button type="button" class="btn btn-default" data-search-type="date">Search by Date</button>
+      <button type="button" class="btn btn-primary" data-search-type="concert-artist">Search by Artist</button>
+      <button type="button" class="btn btn-default" data-search-type="concert-location">Search by Location</button>
+      <button type="button" class="btn btn-default" data-search-type="concert-date">Search by Date</button>
     </div>
   </div>
 </div>
 
 <div class="container">
   <p>
-	<form class="form-horizontal">
+	<form class="form-horizontal" id="search-form">
   	<fieldset>
 	  <div class="form-group">
       <label class="col-md-1 control-label" for="prependedtext"></label>
@@ -19,6 +19,11 @@
         <div class="input-group search-group">
              <span class="input-group-addon addonborder"><span class="glyphicon glyphicon-search bigicon"></span></span>
 	             <input id="searchinput" name="searchinput" type="search" placeholder="Search by Artist" class="form-control input-md">
+        </div>
+        <h2 class="press-enter" style="display: none;">(press enter to search)</h2>
+        <div id="search-results" style="display: none;">
+          <h2><span class="type">...</span> matching your search: "<span class="term"></span>":</h2>
+          <table class="results table"></table>
         </div>
 	    </div>
 	  </div>

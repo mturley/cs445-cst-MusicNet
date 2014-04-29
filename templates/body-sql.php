8e8a4151a9332edceefe4de9cffd825eaef4b35e
Eric Smith <ecsmith@student.umass.edu>
2014-04-29 04:43:36 -0400

    fixes2

--- templates/body-sql.php
+++ templates/body-sql.php
@@ -2,6 +2,7 @@
 <form role="form">
   <div class="form-group">
     <label for="rawsql">Raw SQL Entry</label>
-    <input class="form-control" id="rawsql" placeholder="Enter SQL Query">
+    <input class="form-control input-lg" rows="3" id="rawsql" placeholder="Enter SQL Query">
   </div>
+  <table class="results table"></table>
 </div>

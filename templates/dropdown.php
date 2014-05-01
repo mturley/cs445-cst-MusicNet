0e6206d7ae61dfc0a9f19db91eb9830625bb1402
xianc <xianc@student.umass.edu>
2014-04-30 21:49:49 -0400

    valid links

--- templates/dropdown.php
+++ templates/dropdown.php
@@ -1,15 +1,13 @@
-<div id="drop" style="float: right; v-align:top"> 
+<div id="drop" style="float: right; v-align:top; margin-right:50px"> 
 <ul class="nav nav-pills">
 	<li class="active"><a href="#">Home</a></li>
   <li class="dropdown">
-    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-      link <span class="caret"></span>
+   <a class="dropdown-toggle" data-toggle="dropdown" href="#">Search <span class="caret"></span>
     </a>
     <ul class="dropdown-menu">
-    	<li role="presentation" class="dropdown-header">something</li>
-    	<li role="presentation" class="dropdown-header">More</li>
-    	<li role="presentation" class="divider"></li>
-    	<li role="presentation" class="dropdown-header">Click me?</li>
+    	<li role="presentation" class="dropdown-header"><a href="?page=search">Music</a></li>
+    	<li role="presentation" class="dropdown-header"><a href="?page=friends">People</a></li>
+    	<li role="presentation" class="dropdown-header"><a href="?page=concerts">Events</a></li>
     </ul>
   </li>
 </ul>

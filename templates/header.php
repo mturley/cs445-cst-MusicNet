290101d88f5f92166f4034fd9166f4f80e96b396
xianc <xianc@student.umass.edu>
2014-05-01 00:18:27 -0400

    df

--- templates/header.php
+++ templates/header.php
@@ -70,20 +70,19 @@
             if($logged_in) {
           ?>
           
-              <ul class="nav nav-pills">
-                <li class="active"><a href="musicnet.php">Home</a></li>
+              <div class="navbar-form navbar-right">
+                <a href="#" class="btn btn-primary" href="musicnet.php">Home</a>
                 <div class="dropdown floatright">
-                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">Search <span class="caret"></span>
-                 </a>
+                  <a class="btn btn-primary" data-toggle="dropdown" href="#">Search <span class="caret"></span>
+                  </a>
                   <ul class="dropdown-menu">
                     <li role="presentation" class="dropdown-header"><a href="?page=search">Music</a></li>
                     <li role="presentation" class="dropdown-header"><a href="?page=friends">People</a></li>
                     <li role="presentation" class="dropdown-header"><a href="?page=concerts">Events</a></li>
                   </ul>
                 </div>
-                <li><a href="musicnet.php?page=user&user_id=<?php echo $_SESSION['user_id']; ?>">...</a></li>
-                <li><a href="musicnet.php?page=logout" class="btn btn-info">Sign Out</a></li>
-              </ul>
+                <a class="data-username" href="musicnet.php?page=user&user_id=<?php echo $_SESSION['user_id']; ?>">...</a></li>
+                <a href="musicnet.php?page=logout" class="btn btn-info">Sign Out</a>
               </div>
 
           <?php

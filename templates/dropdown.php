ca6458ab6886fc74fb2f006203fc1efcf57d1770
xianc <xianc@student.umass.edu>
2014-04-30 21:41:40 -0400

    positioning

--- templates/dropdown.php
+++ templates/dropdown.php
@@ -1,10 +1,6 @@
 <div id="drop" style="float: right; v-align:top"> 
 <ul class="nav nav-tabs">
 	<li class="active"><a href="#">Home</a></li>
-	<button class="btn dropdown-toggle sr-only" type="button" id="dropdownMenu1" data-toggle="dropdown">
-    Something
-    <span class="caret"></span>
-  </button>
   <li class="dropdown">
     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
       link <span class="caret"></span>

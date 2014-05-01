b701769ecfaf2c7ee7f19788b1745aa27f00125f
xianc <xianc@student.umass.edu>
2014-05-01 05:44:45 -0400

    useractivity

--- backend.php
+++ backend.php
@@ -314,7 +314,7 @@
       $num_activity = $_GET['num_activity'];
       $user_id = $_SESSION['user_id'];
       try {
-        $q = $db->prepare("select u.user_id from Users u, Ratings r where u.user_id = :user_id and r.user_id=:user_id order by date limit $num_activity");
+        $q = $db->prepare("select r.user_id from Ratings r where r.user_id=:user_id order by date limit 5; $num_activity");
         $q->execute(array(':user_id' => $user_id));
         $response->results = $q->fetchAll();
         $response->message = "User Acitivy returned in results field.";

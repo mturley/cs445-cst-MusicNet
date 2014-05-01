fc897270aa193bb2225ad6a68a6acbec49382b48
xianc <xianc@student.umass.edu>
2014-04-30 21:12:08 -0400

    res path?

--- templates/body-user.php
+++ templates/body-user.php
@@ -19,7 +19,7 @@
     <h2>
       <span class="glyphicon glyphicon-search"></span>&nbsp;
       View User: <?php echo $_GET['user_id']; ?>
-      <? include(echo RES_PATH+"/templates/dropdown..php") ?>
+      <? include(RES_PATH+"/templates/dropdown..php") ?>
     </h2>
   </div>
 </div>

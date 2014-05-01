c3973a5085f1943dc8a7e50b7db6a27daa2dd0b3
xianc <xianc@student.umass.edu>
2014-04-30 21:25:19 -0400

    trying some drop downs

--- templates/dropdown.php
+++ templates/dropdown.php
@@ -1,1 +1,12 @@
-<div id="drop" style="float: right; v-align:top"> dropdown&nbsp; link &nbsp;link</div>
+<div id="drop" style="float: right; v-align:top"> dropdown&nbsp; link &nbsp;
+
+<ul class="nav nav-tabs">
+  <li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+      link <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+    </ul>
+  </li>
+</ul>
+</div>

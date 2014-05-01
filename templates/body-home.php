940497359cbb2cefdb32787b71302a72e06572aa
Mike Turley <mike@miketurley.com>
2014-04-30 23:23:12 -0400

    dropdown integration

--- templates/body-home.php
+++ templates/body-home.php
@@ -4,7 +4,7 @@
 ?>
     <div class="jumbotron floating">
       <div class="container">
-        <h3>Welcome back, <?php echo $_SESSION['user_id']; ?>! </h3>
+        <h3>Welcome back, <span class="data-username">...</span>! </h3>
       </div>
     </div>
 
